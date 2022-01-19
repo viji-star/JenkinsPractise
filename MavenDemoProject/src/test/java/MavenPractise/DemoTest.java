c4a1de984e0b9a39f59288de0f55700d2301b679
Viji <vijilekshmi2708@gmail.com>
2022-01-19 23:14:10 +0530

    Pushing the code to GitHub to run via Jenkins

---
+++ MavenDemoProject/src/test/java/MavenPractise/DemoTest.java
@@ -0,0 +1,43 @@
+package MavenPractise;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class DemoTest {
+
+	@Test
+	public void sum()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+		System.out.println("SUM SUCCESS");
+	}
+	@Test
+	public void sub()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10, b-a);
+		System.out.println("SUB SUCCESS");
+	}
+	
+	@Test
+	public void div()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+		System.out.println("DIV SUCCESS");
+	}
+	
+	@Test
+	public void multi()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, b*a);
+		System.out.println("MULTI SUCCESS");
+	}
+}
